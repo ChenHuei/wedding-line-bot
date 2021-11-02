f272f96ec0ddff6dfcfff66ba5d9bc8c235f3544
chenhuei <aaa830714@gmail.com>
2021-11-02 23:29:38 +0800

    Update constants

--- constants/keyword.ts
+++ constants/keyword.ts
@@ -11,11 +11,26 @@ const KEYWORD_COMPARISON = {
   // 菜單
   今天吃什麼: "menu",
 
+  // 日期
+  婚禮在什麼時候: "date",
+
   // 場地
   場地在哪裡: "location",
 
   // 流程
   流程是什麼: "flow",
+
+  // 婚紗照
+  想看一下婚紗照: "pictures",
+
+  // 問卷
+  我要去哪裡填問卷: "question",
+
+  // 婚禮座位
+  婚禮當天我會坐哪裡: "seat",
+
+  // 祝福新人
+  祝福新人: "cong",
 };
 
 module.exports = {
--- constants/message.ts
+++ constants/message.ts
@@ -51,6 +51,10 @@ const MESSAGE_CONTENT = {
   ],
   location: [
     {
+      type: "text",
+      text: `晶宴婚宴會館－民生館${BREAK_LINE}(台北市中山區民生東路三段 8 號 B2)`,
+    },
+    {
       type: "location",
       title: "晶宴會館 - 民生店",
       address: "台北市中山區民生東路三段 8 號 1 樓",
@@ -62,6 +66,70 @@ const MESSAGE_CONTENT = {
     {
       type: "text",
       text: `這是我們當天的流程${BREAK_LINE}12:00   賓客入席${BREAK_LINE}12:30   午宴用餐${BREAK_LINE}14:30   合照送客`,
+    },
+  ],
+  pictures: [
+    {
+      type: "text",
+      text: "偷偷的搶先給你看幾張婚紗照 σ(o'ω'o)",
+    },
+    {
+      type: "image",
+      originalContentUrl:
+        "https://res.cloudinary.com/dtssrpk5g/image/upload/v1635866335/1_bdgsnz.webp",
+      previewImageUrl:
+        "https://res.cloudinary.com/dtssrpk5g/image/upload/v1635866335/1_bdgsnz.webp",
+    },
+    {
+      type: "image",
+      originalContentUrl:
+        "https://res.cloudinary.com/dtssrpk5g/image/upload/v1635866336/2_qzrezp.webp",
+      previewImageUrl:
+        "https://res.cloudinary.com/dtssrpk5g/image/upload/v1635866336/2_qzrezp.webp",
+    },
+
+    {
+      type: "image",
+      originalContentUrl:
+        "https://res.cloudinary.com/dtssrpk5g/image/upload/v1635866337/3_mwxesf.webp",
+      previewImageUrl:
+        "https://res.cloudinary.com/dtssrpk5g/image/upload/v1635866337/3_mwxesf.webp",
+    },
+  ],
+  date: [
+    {
+      type: "text",
+      text: `我們的喜宴舉辦在 2021年12月11日(六) 午宴${BREAK_LINE}12:00   賓客入席${BREAK_LINE}11:30   迎賓酒會${BREAK_LINE}12:00   賓客入席${BREAK_LINE}12:30   午宴開席${BREAK_LINE}14:30   合照送客`,
+    },
+  ],
+  question: [
+    {
+      type: "text",
+      text: "請點選以下連結",
+    },
+    {
+      type: "text",
+      text: "https://google.com",
+    },
+  ],
+  seat: [
+    {
+      type: "text",
+      text: `不好意思，我們還沒有安排好${BREAK_LINE}等大家表單填寫完畢我們將盡快安排${BREAK_LINE}完成後會再推播給大家喲！`,
+    },
+    {
+      type: "text",
+      text: "還沒填寫表單的話請點以下連結",
+    },
+    {
+      type: "text",
+      text: "https://google.com",
+    },
+  ],
+  cong: [
+    {
+      type: "text",
+      text: "此功能將於婚禮當天開放喲！",
     },
   ],
 };
